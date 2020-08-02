d996d75bb088b1725733564560dfd118f462f254
divakaivan <54508530+divakaivan@users.noreply.github.com>
2020-08-02 12:52:12 +0300

    add modal to bring up a big version of the clicked picture

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import Title from "./components/Title";
 import UploadForm from "./components/UploadForm";
 import ImageGrid from "./components/ImageGrid";
+import Modal from "./components/Modal";
 
 function App() {
+  const [selectedImage, setSelectedImage] = useState(null);
+
   return (
     <div className="App">
       <Title />
       <UploadForm />
-      <ImageGrid />
+      <ImageGrid setSelectedImage={setSelectedImage} />
+      {selectedImage && (
+        <Modal
+          setSelectedImage={setSelectedImage}
+          selectedImage={selectedImage}
+        />
+      )}
     </div>
   );
 }
