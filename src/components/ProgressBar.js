fd35f20a2bdab5ff1b8efd6e40b5190946c79b20
divakaivan <54508530+divakaivan@users.noreply.github.com>
2020-08-02 12:53:54 +0300

    add framer animation to the progress bar

--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import useStorage from "../hooks/useStorage";
+import { motion } from "framer-motion";
 
 const ProgressBar = ({ file, setFile }) => {
   const { url, progress } = useStorage(file);
@@ -8,7 +9,13 @@ const ProgressBar = ({ file, setFile }) => {
     if (url) setFile(null);
   }, [url, setFile]);
 
-  return <div className="progress-bar" style={{ width: progress + "%" }}></div>;
+  return (
+    <motion.div
+      initial={{ width: 0 }}
+      animate={{ width: progress + "%" }}
+      className="progress-bar"
+    ></motion.div>
+  );
 };
 
 export default ProgressBar;
